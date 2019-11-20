3cb9911175988ac6389163eceb8447d82699470e
takigawa <423syouta@gmail.com>
2019-11-20 22:35:19 +0900

    config data fix

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -3,12 +3,12 @@
     <v-row>
       <v-row justify="space-around">
         <v-col cols="10" class="mt-12">
-          <v-select :items="style" label="サンマ or ヨンマ" solo></v-select>
-          <v-slider v-model="betMoney" label="掛け金" min="1" max="20" thumb-label="always"></v-slider>
-          <v-slider v-model="flyingPrice" label="ハコ" min="1" max="20" thumb-label="always"></v-slider>
-          <v-slider v-model="tip" label="チップ" min="1" max="10" thumb-label="always"></v-slider>
-          <v-select :items="uma" label="馬" solo></v-select>
-          <v-btn>設定保存</v-btn>
+          <v-select :items="style" v-model="config.style" label="サンマ or ヨンマ" solo></v-select>
+          <v-slider v-model="config.betMoney" label="掛け金" min="1" max="20" thumb-label="always"></v-slider>
+          <v-slider v-model="config.flyingPrice" label="ハコ値" min="1" max="20" thumb-label="always"></v-slider>
+          <v-slider v-model="config.tip" label="チップ" min="1" max="10" thumb-label="always"></v-slider>
+          <v-select :items="uma" v-model="config.uma" label="馬" solo></v-select>
+          <v-btn @click="checkConfig()">設定保存</v-btn>
         </v-col>
       </v-row>
     </v-row>
@@ -19,12 +19,13 @@
 export default {
   data() {
     return {
-      betMoney: 0,
-      flyingPrice: 0,
-      tip: 0,
       style: ["サンマ", "ヨンマ", "サンマ(4人)"],
-      uma: ["15/5","20/0"]
+      uma: ["15/5", "20/0"],
+      config: []
     };
+  },
+  methods: {
+    checkConfig() {}
   }
 };
 </script>
